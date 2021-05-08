9603779361a945a40c685788381c9ab891844c59
Michael Martins <michaelfm21@gmail.com>
2021-05-08 14:22:15 -0300

    capitulo 11

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -7,11 +7,15 @@ class DateConverter {
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 
-    static paraData(texto) {        
-        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
-            throw new Error('Deve estar no formato aaaa-mm-dd');
+    static paraData(texto) {                
+        if (!/\d{2}\/\d{2}\/\d{4}/.test(texto)) {
+            throw new DataInvalidaException();
         }
 
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        // return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+
+        return new Date(...texto.split('/')
+            .reverse()
+            .map((item, indice) => item - indice % 2));
     }
 }
