95b61d85a09349d26fb70a5fb15727cd23aa309a
Michael Martins <michaelfm21@gmail.com>
2021-05-15 22:20:11 -0300

    capitulo 16

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -37,14 +37,34 @@ class NegociacaoController {
         this._mensagemView = new MensagemView('#mensagemView');
         this._mensagemView.update(this._mensagem);
 
+        this._init();        
+
     };
+
+    _init() {        
+        getNegociacaoDao()
+        .then(dao => dao.listaTodos())
+        .then(negociacoes =>
+            negociacoes.forEach(negociacao =>
+                this._negociacoes.adiciona(negociacao)))
+        .catch(err => this._mensagem.texto = err);
+    }
 
     adiciona(event) {
        try {
             event.preventDefault();
-            this._negociacoes.adiciona(this._criaNegociacao());
-            this._mensagem.texto = 'Negociação adicionada com sucesso';        
-            this._limpaFormulario();
+
+            const negociacao = this._criaNegociacao();
+        
+            getNegociacaoDao()
+            .then(dao => dao.adiciona(negociacao))
+            .then(() => {
+                this._negociacoes.adiciona(this._criaNegociacao());
+                this._mensagem.texto = 'Negociação adicionada com sucesso';        
+                this._limpaFormulario();
+            })
+            .catch(err => this._mensagem.texto = err);
+            
        } catch(err) {
             console.log(err);
             console.log(err.stack);
@@ -58,8 +78,13 @@ class NegociacaoController {
     }  
 
     apaga() {
-        this._negociacoes.esvazia();        
-        this._mensagem.texto = 'Negociações apagadas com sucesso';        
+        getNegociacaoDao()
+        .then(dao => dao.apagaTodos())
+        .then(() => {
+            this._negociacoes.esvazia();
+            this._mensagem.texto = 'Negociações apagadas com sucesso';
+        })
+        .catch(err => this._mensagem.texto = err);
     }
 
     _limpaFormulario() {
